d6834caa84667663b622279a072a79199bcee7ab
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-12-07 01:36:37 -0500

    summ

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from '../SharedLayout/SharedLayout';
+
+const Home = lazy(() => import('../../pages/Home/Home'));
+const MoviesDetails = lazy(() =>
+  import('../../pages/MoviesDetails/MoviesDetails')
+);
+const Movies = lazy(() => import('../../pages/Movies/Movies'));
+const Cast = lazy(() => import('../Cast/Cast'));
+const Reviews = lazy(() => import('../Reviews/Reviews'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MoviesDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
+//API Key: 52aed080f92e177ae510784bfb4b51ec
+//https://api.themoviedb.org/3/movie/550?api_key=52aed080f92e177ae510784bfb4b51ec
